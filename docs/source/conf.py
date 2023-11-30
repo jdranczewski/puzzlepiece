ac92bdac7a3ade766bbffb460ef88c2dade24b69
jdranczewski <domdlapsa@gmail.com>
2023-11-30 21:03:30 +0000

    Update to docs conf Readthedocs was having issues with PyQt6, so we now mock the module

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,6 +29,8 @@ exclude_patterns = []
 
 autodoc_member_order = 'bysource'
 
+autodoc_mock_imports = ["pyqtgraph", "QtPy", "numpy"]
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
